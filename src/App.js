79766595d0cbe27b26d4a2747f8ea1ccf08fac04
Manish Kumar Verma <manishkumar.v@sintecmedia.com>
2025-02-04 15:29:07 +0530

    React: added timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [dateNow, setDateNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateNow(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Current time: {dateNow.toLocaleTimeString()}</p>
         <p>Namaste to my React Lovers ❤️</p>
         <a
           className="App-link"
